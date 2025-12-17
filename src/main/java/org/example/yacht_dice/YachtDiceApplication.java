0b46a1b01f2a563f83c33548cbf9e6a0ceb42f4f
이종환 (인턴) <ghksqkdwod0@innogrid.com>
2025-12-17 17:08:53 +0900

    init

---
+++ src/main/java/org/example/yacht_dice/YachtDiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.yacht_dice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YachtDiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YachtDiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/yacht_dice/YachtDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.yacht_dice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YachtDiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
